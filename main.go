a2c3a96750fab3e342f1c53a3d49af1641e48b76
Martin Danko <martin@nuclia.com>
2023-10-14 20:31:01 +0200

    Refactor, use CLI flags

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log/slog"
@@ -23,105 +24,108 @@ import (
 )
 
 const (
-	eksClusterIdHeader = "x-k8s-aws-id"
+	eksClusterIdHeader = "x-k8s-aws-id" // Header name identifying EKS cluser in STS getCallerIdentity call
 	// The sts GetCallerIdentity request is valid for 15 minutes regardless of this parameters value after it has been
 	// signed, but we set this unused parameter to 60 for legacy reasons (we check for a value between 0 and 60 on the
 	// server side in 0.3.0 or earlier).  IT IS IGNORED.  If we can get STS to support x-amz-expires, then we should
 	// set this parameter to the actual expiration, and make it configurable.
-	requestPresignParam = 60
-	// eksClusterName      = "seom"
-	// The actual token expiration (presigned STS urls are valid for 15 minutes after timestamp in x-amz-date).
-	presignedURLExpiration = 15 * time.Minute
-	tokenV1Prefix          = "k8s-aws-v1."
+	requestPresignParam    = 60
+	presignedURLExpiration = 15 * time.Minute // The actual token expiration (presigned STS urls are valid for 15 minutes after timestamp in x-amz-date).
+	tokenV1Prefix          = "k8s-aws-v1."    // Prefix of a token in client.authentication.k8s.io/v1beta1 ExecCredential
 )
 
+var logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+// Creates GCP metadata client
 func gcpMetadataClient() *metadata.Client {
-	c := metadata.NewClient(&http.Client{Timeout: 1 * time.Second, Transport: userAgentTransport{
-		userAgent: "my-user-agent",
-		base:      http.DefaultTransport,
-	}})
+	c := metadata.NewClient(&http.Client{Timeout: 1 * time.Second})
 	return c
 }
 
-// This example demonstrates how to use your own transport when using this package.
-func main() {
-	awsAssumeRoleArn := os.Args[1]
-	eksClusterName := os.Args[2]
-	stsRegion := os.Args[3]
-
-	ctx := context.Background()
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-
-	// Retrieve information from GCP API
-	c := gcpMetadataClient()
-
+// Constucts AWs session identifier from GCP metadata infrmation.
+// This implementation uses concentration of  GCP project ID and machine hostname
+func createSessionIdentifier(c *metadata.Client) (string, error) {
 	projectId, err := c.ProjectID()
 	if err != nil {
-		logger.Error("Couldn't connect to GCP metadata server")
-		os.Exit(1)
+		logger.Error("Couldn't fetch ProjectId from GCP metadata server")
+		return "", err
 	}
 
 	hostname, err := c.Hostname()
 	if err != nil {
-		logger.Error("Couldn't connect to GCP metadata server")
-		os.Exit(1)
+		logger.Error("Couldn't fetch Hostname from GCP metadata server")
+		return "", err
 	}
 
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?format=standard&audience=gcp", nil)
+	return (fmt.Sprintf("%s-%s", projectId, hostname)[:32]), nil
+}
+
+// Retrieves GCE identity token (JWT) and retuens [customIdentityTokenRetriever instance
+// containing the token. This is to be then used in [stscreds.NewWebIdentityRoleProvider]
+// function.
+func gcpRetrieveGCEVMToken(ctx context.Context) (customIdentityTokenRetriever, error) {
+	url := "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?format=full&audience=gcp"
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return customIdentityTokenRetriever{token: nil}, fmt.Errorf("http.NewRequest: %w", err)
+	}
 	req.Header.Set("Metadata-Flavor", "Google")
-	response, _ := client.Do(req)
-
-	body, err := io.ReadAll(response.Body)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		logger.Error("Couldn't connect to GCP metadata server %s", err)
-		os.Exit(1)
+		return customIdentityTokenRetriever{token: nil}, fmt.Errorf("client.Do: %w", err)
 	}
-	response.Body.Close()
-
-	homeDir, err := os.UserHomeDir()
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return customIdentityTokenRetriever{token: nil}, fmt.Errorf("status code %d", resp.StatusCode)
+	}
+	b, err := io.ReadAll(resp.Body)
 	if err != nil {
-		logger.Error("%s", err)
+		return customIdentityTokenRetriever{token: nil}, fmt.Errorf("io.ReadAll: %w", err)
+	}
+	gcpMetadataToken := customIdentityTokenRetriever{token: b}
+	return gcpMetadataToken, nil
+}
+
+func main() {
+	awsAssumeRoleArn := flag.String("rolearn", "", "AWS role ARN to assume (required)")
+	eksClusterName := flag.String("cluster", "", "AWS cluster name for which we create credentials (required)")
+	stsRegion := flag.String("stsregion", "us-east-1", "AWS STS region to which requests are made (optional)")
+
+	flag.Parse()
+	if *awsAssumeRoleArn == "" || *eksClusterName == "" {
+		flag.Usage()
 		os.Exit(1)
 	}
 
-	f, err := os.CreateTemp(homeDir, "token")
+	ctx := context.Background()
 
+	sessionIdentifier, err := createSessionIdentifier(gcpMetadataClient())
 	if err != nil {
-		logger.Error("%s", err)
+		logger.Error("Failed to create session identifier from GCP metadata, %s" + err.Error())
 		os.Exit(1)
 	}
 
-	defer f.Close()
-
-	_, err2 := f.Write(body)
-
-	if err2 != nil {
-		logger.Error("%s", err2)
-		os.Exit(1)
-	}
-
-	// Set custom session identifier
-	sessionIdentifier := fmt.Sprintf("%s-%s", projectId, hostname)[:32]
-
-	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(stsRegion))
+	assumeRoleCfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(*stsRegion))
 	if err != nil {
 		logger.Error("failed to load default AWS config, %s" + err.Error())
 		os.Exit(1)
 	}
 
-	stsAssumeClient := sts.NewFromConfig(cfg)
+	gcpMetadataToken, err := gcpRetrieveGCEVMToken(ctx)
+	if err != nil {
+		logger.Error("Failed to get JWT token from GCP metadata, %s" + err.Error())
+		os.Exit(1)
+	}
 
+	stsAssumeClient := sts.NewFromConfig(assumeRoleCfg)
 	awsCredsCache := aws.NewCredentialsCache(stscreds.NewWebIdentityRoleProvider(
 		stsAssumeClient,
-		awsAssumeRoleArn,
-		stscreds.IdentityTokenFile(f.Name()),
+		*awsAssumeRoleArn,
+		gcpMetadataToken,
 		func(o *stscreds.WebIdentityRoleOptions) {
 			o.RoleSessionName = sessionIdentifier
 		}),
 	)
-
-	defer os.Remove(f.Name())
 
 	awsCredentials, err := awsCredsCache.Retrieve(ctx)
 	if err != nil {
@@ -129,7 +133,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	cfg2, err := config.LoadDefaultConfig(ctx, config.WithRegion(stsRegion),
+	eksSignerCfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(*stsRegion),
 		config.WithCredentialsProvider(credentials.StaticCredentialsProvider{
 			Value: awsCredentials,
 		}),
@@ -139,12 +143,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	stsClient := sts.NewFromConfig(cfg2)
+	stsClient := sts.NewFromConfig(eksSignerCfg)
 
 	presignclient := sts.NewPresignClient(stsClient)
 	presignedURLString, err := presignclient.PresignGetCallerIdentity(ctx, &sts.GetCallerIdentityInput{}, func(opt *sts.PresignOptions) {
 		opt.Presigner = newCustomHTTPPresignerV4(opt.Presigner, map[string]string{
-			eksClusterIdHeader: eksClusterName,
+			eksClusterIdHeader: *eksClusterName,
 			"X-Amz-Expires":    "60",
 		})
 	})
@@ -153,18 +157,6 @@ func main() {
 	// Set token expiration to 1 minute before the presigned URL expires for some cushion
 	tokenExpiration := time.Now().Local().Add(presignedURLExpiration - 1*time.Minute)
 	_, _ = fmt.Fprint(os.Stdout, formatJSON(token, tokenExpiration))
-}
-
-// userAgentTransport sets the User-Agent header before calling base.
-type userAgentTransport struct {
-	userAgent string
-	base      http.RoundTripper
-}
-
-// RoundTrip implements the http.RoundTripper interface.
-func (t userAgentTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	req.Header.Set("User-Agent", t.userAgent)
-	return t.base.RoundTrip(req)
 }
 
 func formatJSON(token string, expiration time.Time) string {
@@ -181,6 +173,14 @@ func formatJSON(token string, expiration time.Time) string {
 	}
 	enc, _ := json.Marshal(execInput)
 	return string(enc)
+}
+
+type customIdentityTokenRetriever struct {
+	token []byte
+}
+
+func (obj customIdentityTokenRetriever) GetIdentityToken() ([]byte, error) {
+	return obj.token, nil
 }
 
 type customHTTPPresignerV4 struct {
